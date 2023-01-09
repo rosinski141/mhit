efa655780a1a22d35ff75d9aa89cc01895f32013
Mateusz Rosinski <rosinski141@hotmail.com>
2023-01-09 01:55:39 +0000

    Match history showing with kda

--- app/Http/Controllers/NavigationController.php
+++ app/Http/Controllers/NavigationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use GuzzleHttp;
+use File;
 
 
 class NavigationController extends Controller
@@ -12,7 +13,7 @@ class NavigationController extends Controller
     {
         $client = new GuzzleHttp\Client();
         $stream = "";
-        $api_key = 'RGAPI-a776d72a-0905-4678-8dac-36830dd40f2e';
+        $api_key = 'RGAPI-9b0828c8-571a-402c-817b-96e5f09df4d1';
         
         if($request->accountServer == "EUW") {
             $stream = $client->get('https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' . $request->username . '?api_key=' . $api_key);
@@ -21,22 +22,39 @@ class NavigationController extends Controller
         $userdata = $stream->getBody()->getContents(); 
         $userid = json_decode($userdata)->id;
         $puuid = json_decode($userdata)->puuid;
-      
+        $username = json_decode($userdata)->name;
         $stream = $client->get('https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/' . $userid . '?api_key=' . $api_key);
         $ranks = json_decode($data = $stream->getBody()->getContents());
-
-        $stream = $client->get('https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/'  . $puuid . '/ids?start=0&count=2&api_key=' . $api_key);
+        $emblems_array = File::files(public_path() . "/ranked-emblems/");
+        $tier = strtolower($ranks[1]->tier);
+        //check if rank is solo duo or felx.
+        $emblem_path = "";
+        foreach($emblems_array as $emblem) {
+            $current_filename = strtolower($emblem->getFilename());
+            $stripped_filename = substr($current_filename, strpos($current_filename, "_") + 1);
+            $stripped_filename = str_replace(".png", "", $stripped_filename);
+            if($tier == $stripped_filename) {
+                $emblem_path = $emblem->getRealPath();
+            }
+        }
+        $emblem_path = "." . str_replace(public_path(), "", $emblem_path);
+        $stream = $client->get('https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/'  . $puuid . '/ids?start=0&count=10&api_key=' . $api_key);
         $matches = json_decode($data = $stream->getBody()->getContents());
         
-        $matchData = [];
+        $match_data = [];
         foreach($matches as $match) {
             $stream =  $client->get('https://europe.api.riotgames.com/lol/match/v5/matches/'  . $match . '?api_key=' . $api_key);
 
-            $matchData[] = [
-                json_decode($data = $stream->getBody()->getContents())
-            ];
+            
+            $data = json_decode($stream->getBody()->getContents());
+            foreach($data->info->participants as $participant) {
+                if($participant->summonerName == $username) {
+                    $match_data[] = $participant;
+                }
+            }
+        
         }
-    
-        return view('player.search', compact("matchData"));
+       
+        return view('player.search', compact("match_data", "ranks", "emblem_path", "username"));
     }
 }
--- resources/views/player/search.blade.php
+++ resources/views/player/search.blade.php
@@ -1,11 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="d-flex justify-content-center"> 
-        <h1> Stats </h1>
-   
-        @foreach($matchData as $match) 
-            {{ dd($match[0]->info) }}
+    <img style="width:200px;height:250px"src="{{$emblem_path}}">
+    <h2 style="display:inline-block">{{$username}}</h2>
+    <div class="justify-content-center"> 
+        
+        @foreach($match_data as $match) 
+           
+            @if($match->win == "true")
+                <div style="background-color:lightblue" class="card">
+              
+                
+           
+            @else
+                <div style="background-color:#F08080" class="card">
+             
+            @endif
+
+            <h3>KDA: {{$match->kills}}/{{$match->deaths}}/{{$match->deaths}}</h3>
+            </div>
         @endforeach
     </div>
 @endsection
