a144389a8610036274cfbc2f854cd4065167b4fa
Mateusz Rosinski <rosinski141@hotmail.com>
2023-01-16 08:04:59 +0000

    Working match history

--- app/Http/Controllers/NavigationController.php
+++ app/Http/Controllers/NavigationController.php
@@ -13,31 +13,53 @@ class NavigationController extends Controller
     {
         $client = new GuzzleHttp\Client();
         $stream = "";
-        $api_key = 'RGAPI-9b0828c8-571a-402c-817b-96e5f09df4d1';
-        
+        $api_key = env("API_KEY");
+        $league_patch = env("LEAGUE_PATCH");
+        $solo_tier = "";
+        $flex_tier = "";
+        $emblem_path = "";
+        $flex_emblem_path = "";
+        $flex_rank = "";
+        $solo_rank = "";
+
         if($request->accountServer == "EUW") {
             $stream = $client->get('https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' . $request->username . '?api_key=' . $api_key);
         }
-
+        
         $userdata = $stream->getBody()->getContents(); 
+        
         $userid = json_decode($userdata)->id;
         $puuid = json_decode($userdata)->puuid;
-        $username = json_decode($userdata)->name;
+        $user = json_decode($userdata);
         $stream = $client->get('https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/' . $userid . '?api_key=' . $api_key);
         $ranks = json_decode($data = $stream->getBody()->getContents());
+     
         $emblems_array = File::files(public_path() . "/ranked-emblems/");
-        $tier = strtolower($ranks[1]->tier);
-        //check if rank is solo duo or felx.
-        $emblem_path = "";
+        foreach($ranks as $rank) {
+            if($rank->queueType == "RANKED_SOLO_5x5") {
+                $solo_tier = strtolower($rank->tier);
+                $solo_rank = $rank;
+            } else {
+                $flex_tier = strtolower($rank->tier);
+                $flex_rank = $rank;
+            }
+        }
+       
         foreach($emblems_array as $emblem) {
             $current_filename = strtolower($emblem->getFilename());
             $stripped_filename = substr($current_filename, strpos($current_filename, "_") + 1);
             $stripped_filename = str_replace(".png", "", $stripped_filename);
-            if($tier == $stripped_filename) {
+            if($solo_tier == $stripped_filename) {
                 $emblem_path = $emblem->getRealPath();
             }
+            if($flex_tier == $stripped_filename) {
+                $flex_emblem_path = $emblem->getRealPath();
+            }
         }
+     
         $emblem_path = "." . str_replace(public_path(), "", $emblem_path);
+        $flex_emblem_path = "." . str_replace(public_path(), "", $flex_emblem_path);
+
         $stream = $client->get('https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/'  . $puuid . '/ids?start=0&count=10&api_key=' . $api_key);
         $matches = json_decode($data = $stream->getBody()->getContents());
         
@@ -46,15 +68,29 @@ class NavigationController extends Controller
             $stream =  $client->get('https://europe.api.riotgames.com/lol/match/v5/matches/'  . $match . '?api_key=' . $api_key);
 
             
-            $data = json_decode($stream->getBody()->getContents());
+            $data = json_decode($stream->getBody()->getContents());     
+           
             foreach($data->info->participants as $participant) {
-                if($participant->summonerName == $username) {
-                    $match_data[] = $participant;
+                // Fixing champion name so they appear on data dragon link
+                if($participant->championName == "FiddleSticks") {
+                    $participant->championName = "Fiddlesticks";
                 }
-            }
+                $items[] = $participant->item0;
+                $items[] = $participant->item1;
+                $items[] = $participant->item2;
+                $items[] = $participant->item3;
+                $items[] = $participant->item4;
+                $items[] = $participant->item5;
+                $items[] = $participant->item6;
+                
+                $participant->items = $items;
+                $items = [];
+            }      
+
+            $match_data[] = $data->info; 
         
         }
        
-        return view('player.search', compact("match_data", "ranks", "emblem_path", "username"));
+        return view('player.search', compact("match_data", "ranks", "emblem_path", "user", "solo_rank", "flex_rank", "flex_emblem_path", "league_patch"));
     }
 }
